8db8da7b6f95717bcc5231a905860ab2ef1a219c
Mats Andreassen <matsand@live.no>
2020-11-14 17:09:25 +0100

    Updated example config

--- example/src/config.js
+++ example/src/config.js
@@ -2,7 +2,7 @@ export const config = {
   auth: {
     clientId: '58e282d0-b89b-4d06-b188-a3761389c33d',
     authority: 'https://login.microsoftonline.com/08f3813c-9f29-482f-9aec-16ef7cbf477a',
-    redirectUri: 'http://localhost:3000',
+    redirectUri: process.env.AUTH_REDIRECT_URL || 'http://localhost:3000',
     postLogoutRedirectUri: 'https://github.com/vtfk/react-msal'
   },
   cache: {
