8a142593c58b5289063d936815f61c8e73e5cf3d
Mats Andreassen <matsand@live.no>
2020-11-14 16:15:36 +0100

    Export MsalProvider and useSession

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import styles from './styles.module.css'
+import { MsalProvider, useSession } from './lib/auth-provider'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export {
+  MsalProvider,
+  useSession
 }
