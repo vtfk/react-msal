d61537130330e04852db982cf3b224121a883866
Mats Andreassen <matsand@live.no>
2020-11-14 16:14:55 +0100

    Added default config

---
+++ src/config.js
@@ -0,0 +1,24 @@
+export default {
+  auth: {
+    clientId: process.env.AUTH_CLIENT_ID,
+    authority: process.env.AUTH_AUTHORITY,
+    redirectUri: process.env.AUTH_REDIRECT_URL,
+    postLogoutRedirectUri: process.env.AUTH_POST_LOGOUT_URL || 'https://vtfk.no'
+  },
+  cache: {
+    cacheLocation: process.env.AUTH_CACHE_LOCATION || 'sessionStorage',
+    storeAuthStateInCookie: process.env.AUTH_STATE_IN_COOKIE === 'true' || false
+  },
+  userInfoUrl: process.env.AUTH_USER_INFO_URL || 'https://graph.microsoft.com/v1.0/me?$select=userPrincipalName,onPremisesSamaccountName,givenName,surname,displayName',
+  isMock: process.env.AUTH_IS_MOCK || process.env.REACT_APP_IS_MOCK || false,
+  mockUser: process.env.AUTH_MOCK_USER ? JSON.parse(process.env.AUTH_MOCK_USER) : {
+    displayName: 'Trine Testesen',
+    givenName: 'Trine',
+    name: 'Trine Testesen',
+    onPremisesSamAccountName: 'tri0308',
+    surname: 'Testesen',
+    tenantId: '08f3813c-9f29-482f-9aec-16ef7cbf477a',
+    userPrincipalName: 'trine.testesen@vtfk.no',
+    username: 'trine.testesen@vtfk.no'
+  }
+}
