25172bf21a8ef20dec98bd074daefe66fa6a91f4
Mats Andreassen <matsand@live.no>
2020-11-14 16:54:51 +0100

    Updates

---
+++ precache-manifest.522766ccc9860975614d084d3b2dbce6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b73fbe879cc28dd96506a8c9a5f7bdf2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a6b662ed1bf60980fc94",
+    "url": "./static/css/main.1fb2c4d8.chunk.css"
+  },
+  {
+    "revision": "99281081142f42fd5e2f",
+    "url": "./static/js/2.9093c507.chunk.js"
+  },
+  {
+    "revision": "efee83b1c8fc2bd919e03bbbbdcfdfae",
+    "url": "./static/js/2.9093c507.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6b662ed1bf60980fc94",
+    "url": "./static/js/main.69e89e80.chunk.js"
+  },
+  {
+    "revision": "d5c828c46256bedfb345",
+    "url": "./static/js/runtime-main.e318bd1c.js"
+  }
+]);
